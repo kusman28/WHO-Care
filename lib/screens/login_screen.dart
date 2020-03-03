5d6feac11f0ccfd2284ccaf039348f45e68cc86d
K. Usman <kusman758@gmail.com>
2020-03-04 00:13:04 +0800

    Login with Firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
 
-class LoginScreen extends StatelessWidget {
+import 'package:who_care/pages/home.dart';
+
+class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String _email, _password;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,81 +27,113 @@ class LoginScreen extends StatelessWidget {
           },
         ),
       ),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        color: Theme.of(context).primaryColor,
-        width: double.infinity,
-        child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Image.asset('assets/images/logo2.png', height: 200,),
-                TextField(
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Username',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    )
-                  )
+      body: Form(
+        key: _formkey,
+          child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return Container(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          color: Theme.of(context).primaryColor,
+          width: double.infinity,
+          child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
                 ),
-                SizedBox(height: 20,),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Image.asset('assets/images/logo2.png', height: 200,),
+                  TextFormField(
+                    validator: (input) {
+                      if(input.isEmpty) {
+                        return 'Please enter your email.';
+                      }
+                    },
+                    onSaved: (input) => _email = input,
+                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.all(15),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      )
                     )
-                  )
-                ),
-                SizedBox(height: 20,),
-                FlatButton( 
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontSize: 20,
                   ),
-                ),
-                shape: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                padding: const EdgeInsets.all(15),
-                textColor: Colors.white,
-                onPressed: () {},
-                ),
-              ],
-          ),
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    validator: (input) {
+                      if(input.length < 6) {
+                        return 'You must have atleast 6 characters.';
+                      }
+                    },
+                    onSaved: (input) => _password = input,
+                    obscureText: true,
+                    style: TextStyle(fontSize: 18, color: Colors.black54),
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.all(15),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(5),
+                      )
+                    )
+                  ),
+                  SizedBox(height: 20,),
+                  FlatButton( 
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  shape: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width: 2),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  padding: const EdgeInsets.all(15),
+                  textColor: Colors.white,
+                  onPressed: signIn,
+                  ),
+                ],
             ),
+              ),
+          ),
+        );
+          }
         ),
-      );
-        }
       )
     );
+  }
+
+  Future<void> signIn() async {
+
+    final formState = _formkey.currentState;
+
+    if(formState.validate()) {
+      formState.save();
+      try{
+
+      AuthResult user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => Home(user: user.user)));
+      }catch(e){
+        print(e.message);
+      }
+    } 
+
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:who_care/screens/login_screen.dart';
 
-class SignupScreen extends StatelessWidget {
+import 'dart:async';
+
+class SignupScreen extends StatefulWidget {
   static const routeName = '/signup';
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  String _email, _password;
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,118 +27,116 @@ class SignupScreen extends StatelessWidget {
           },
         ),
       ),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 30),
-        color: Theme.of(context).primaryColor,
-        width: double.infinity,
-        child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Image.asset('assets/images/logo2.png', height: 200,),
-                TextField(
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Email Address',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    )
-                  )
-                ),
-                SizedBox(height: 20,),
-                TextField(
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Username',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    )
-                  )
-                ),
-                SizedBox(height: 20,),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    )
-                  )
-                ),
-                SizedBox(height: 20,),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(fontSize: 18, color: Colors.black54),
-                  decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.all(15),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.circular(5),
-                    )
-                  )
-                ),
-                SizedBox(height: 20,),
-                FlatButton( 
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontSize: 20,
+      body: Form(
+        key: _formkey,
+          child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return Container(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              color: Theme.of(context).primaryColor,
+              width: double.infinity,
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight: viewportConstraints.maxHeight,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Image.asset(
+                        'assets/images/logo2.png',
+                        height: 200,
+                      ),
+                      TextFormField(
+                          validator: (input) {
+                            if (input.isEmpty) {
+                              return 'Please enter your email.';
+                            }
+                          },
+                          onSaved: (input) => _email = input,
+                          style: TextStyle(fontSize: 18, color: Colors.black54),
+                          decoration: InputDecoration(
+                              hintText: 'Email Address',
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(5),
+                              ))),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                          validator: (input) {
+                            if (input.length < 6) {
+                              return 'You must have atleast 6 characters.';
+                            }
+                          },
+                          onSaved: (input) => _password = input,
+                          obscureText: true,
+                          style: TextStyle(fontSize: 18, color: Colors.black54),
+                          decoration: InputDecoration(
+                              hintText: 'Password',
+                              filled: true,
+                              fillColor: Colors.white,
+                              contentPadding: const EdgeInsets.all(15),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(5),
+                              ))),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FlatButton(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        shape: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        padding: const EdgeInsets.all(15),
+                        textColor: Colors.white,
+                        onPressed: signUp,
+                      ),
+                    ],
                   ),
                 ),
-                shape: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width: 2),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                padding: const EdgeInsets.all(15),
-                textColor: Colors.white,
-                onPressed: () {},
-                ),
-              ],
-          ),
-            ),
+              ),
+            );
+          },
         ),
-      );
-        },
       ),
     );
+  }
+
+  Future<void> signUp() async{
+    final formState = _formkey.currentState;
+    if(formState.validate()){
+      formState.save();
+      try{
+        AuthResult user = await FirebaseAuth.instance.createUserWithEmailAndPassword(password: _password, email: _email);
+        // FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+        user.user.sendEmailVerification(); 
+        
+        // Navigator.of(context).pop();
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      }catch(e){
+        print(e.message);
+      }
+    }
   }
 }
