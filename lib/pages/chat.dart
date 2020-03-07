47b67f59e6a98c68f3f32444f22594431694d592
K. Usman <kusman758@gmail.com>
2020-03-08 01:15:03 +0800

    chatbot matug na kita

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -105,7 +105,8 @@ class ChatMessage extends StatelessWidget {
     return <Widget>[
       new Container(
         margin: const EdgeInsets.only(right: 16.0),
-        child: new CircleAvatar(child: new Text('B')),
+        // child: new CircleAvatar(child: new Text('B')),
+        child: new CircleAvatar(child: new Image.asset("images/AppCon.png")),
       ),
       new Expanded(
         child: new Column(
